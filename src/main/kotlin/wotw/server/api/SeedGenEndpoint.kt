7b3a2463c352ddd4563a9345c036625fe5066e4d
Timo Schwarzer <timo@schwarzer.dev>
2022-03-20 20:02:12 +0100

    Set content type for headers

--- src/main/kotlin/wotw/server/api/SeedGenEndpoint.kt
+++ src/main/kotlin/wotw/server/api/SeedGenEndpoint.kt
@@ -55,6 +55,7 @@ class SeedGenEndpoint(server: WotwBackendServer) : Endpoint(server) {
                 throw BadRequestException("Invalid header requested")
             }
 
+            call.response.headers.append("Content-Type", "application/vnd.ori-rando.header")
             call.respond(HttpStatusCode.OK, headerFile.toFile().readText())
         }
 
