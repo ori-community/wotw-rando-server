074bfc799709eed792c326ba1e59d2def19aa1c9
Timo Schwarzer <timo@schwarzer.dev>
2024-04-12 20:52:30 +0200

    Set minimum client version to 4.1.5

--- src/main/kotlin/wotw/server/constants/Compatibility.kt
+++ src/main/kotlin/wotw/server/constants/Compatibility.kt
@@ -2,4 +2,4 @@ package wotw.server.constants
 
 import io.github.z4kn4fein.semver.constraints.toConstraint
 
-val SUPPORTED_CLIENT_VERSIONS = "^4.1.2".toConstraint()
+val SUPPORTED_CLIENT_VERSIONS = "^4.1.5".toConstraint()
