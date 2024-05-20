6cea9a0775fa94bd002474bf8b456760a5e791ec
Timo Schwarzer <timo@schwarzer.dev>
2024-05-20 13:31:21 +0200

    Bump minimum compatible version to 4.10.4

--- src/main/kotlin/wotw/server/constants/Compatibility.kt
+++ src/main/kotlin/wotw/server/constants/Compatibility.kt
@@ -2,4 +2,4 @@ package wotw.server.constants
 
 import io.github.z4kn4fein.semver.constraints.toConstraint
 
-val SUPPORTED_CLIENT_VERSIONS = "^4.10.3".toConstraint()
+val SUPPORTED_CLIENT_VERSIONS = "^4.10.4".toConstraint()
