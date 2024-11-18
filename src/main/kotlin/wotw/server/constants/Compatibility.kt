50fb73c27aeb7dc262540878d6fc6aeab2d8a0f7
Timo Schwarzer <timo@schwarzer.dev>
2024-11-18 21:41:02 +0100

    Bump minimal client version to 4.25.0

--- src/main/kotlin/wotw/server/constants/Compatibility.kt
+++ src/main/kotlin/wotw/server/constants/Compatibility.kt
@@ -2,4 +2,4 @@ package wotw.server.constants
 
 import io.github.z4kn4fein.semver.constraints.toConstraint
 
-val SUPPORTED_CLIENT_VERSIONS = "^4.23.3".toConstraint()
+val SUPPORTED_CLIENT_VERSIONS = "^4.25.0".toConstraint()
