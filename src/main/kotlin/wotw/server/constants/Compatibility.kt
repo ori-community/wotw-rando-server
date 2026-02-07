a3aba35e34bc8e21286b40ea96c579bea84578f2
Timo Schwarzer <timo@schwarzer.dev>
2026-02-07 01:29:00 +0100

    Bump protocol version to 5.0.0-alpha.0

--- src/main/kotlin/wotw/server/constants/Compatibility.kt
+++ src/main/kotlin/wotw/server/constants/Compatibility.kt
@@ -2,4 +2,4 @@ package wotw.server.constants
 
 import io.github.z4kn4fein.semver.constraints.toConstraint
 
-val SUPPORTED_CLIENT_VERSIONS = "^4.39.2".toConstraint()
+val SUPPORTED_CLIENT_VERSIONS = "^5.0.0-alpha.0".toConstraint()
