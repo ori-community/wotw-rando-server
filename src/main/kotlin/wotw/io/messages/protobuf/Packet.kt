79b9c6a7f1768dee56462784a80594be73479b82
Timo Schwarzer <timo@schwarzer.dev>
2026-02-08 20:09:46 +0100

    Fix InitGameSyncMessage having two packet IDs

--- src/main/kotlin/wotw/io/messages/protobuf/Packet.kt
+++ src/main/kotlin/wotw/io/messages/protobuf/Packet.kt
@@ -49,7 +49,6 @@ data class Packet(
             3 to typeOf<UberStateUpdateMessage>(),
             4 to typeOf<SyncBingoUniversesMessage>(),
             5 to typeOf<InitGameSyncMessage>(),
-            6 to typeOf<InitGameSyncMessage>(),
             7 to typeOf<UberStateBatchUpdateMessage>(),
             8 to typeOf<MultiverseInfoMessage>(),
             9 to typeOf<AuthenticateMessage>(),
